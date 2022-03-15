0d8d1ee99916c52c406ea69281ff2969dce04bac
soyer <soyer@irl.hu>
2022-03-15 01:33:16 +0100

    kiyoproctrls

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # kiyoproctrls
-Razer Kiyo Pro controls for linux
+Razer Kiyo Pro controls for Linux
+
+It's a standalone Python script to set the Razer Kiyo Pro custom controls (HDR, HDR mode, FoV, AF mode).
+Extracted from the [fmp4streamer](https://github.com/soyersoyer/fmp4streamer) project.
+
+```
+$ python3 kiyoproctrls.py
+usage: python3 kiyoproctrls.py [--help] [-d DEVICE] [--list] [-c CONTROLS]
+
+optional arguments:
+  -h, --help    show this help message and exit
+  -d DEVICE     use DEVICE, default /dev/video0
+  -l, --list    list the controls and values
+  -c CONTROLS   set CONTROLS (eg.: hdr=on,fov=wide)
+
+example:
+  python3 kiyoproctrls.py -c hdr=on,hdr_mode=dark,af_mode=passive,fov=wide
+```
---
+++ kiyoproctrls.py
@@ -0,0 +1,337 @@
+import ctypes, logging, os.path, getopt, sys
+from fcntl import ioctl
+
+# ioctl
+
+_IOC_NRBITS = 8
+_IOC_TYPEBITS = 8
+_IOC_SIZEBITS = 14
+
+_IOC_NRSHIFT = 0
+_IOC_TYPESHIFT = _IOC_NRSHIFT + _IOC_NRBITS
+_IOC_SIZESHIFT = _IOC_TYPESHIFT + _IOC_TYPEBITS
+_IOC_DIRSHIFT = _IOC_SIZESHIFT + _IOC_SIZEBITS
+
+_IOC_WRITE = 1
+_IOC_READ  = 2
+
+def _IOC(dir_, type_, nr, size):
+    return (
+        ctypes.c_int32(dir_ << _IOC_DIRSHIFT).value |
+        ctypes.c_int32(ord(type_) << _IOC_TYPESHIFT).value |
+        ctypes.c_int32(nr << _IOC_NRSHIFT).value |
+        ctypes.c_int32(size << _IOC_SIZESHIFT).value)
+
+def _IOC_TYPECHECK(t):
+    return ctypes.sizeof(t)
+
+def _IOWR(type_, nr, size):
+    return _IOC(_IOC_READ | _IOC_WRITE, type_, nr, _IOC_TYPECHECK(size))
+
+#
+# ioctl structs, codes for UVC extensions
+#
+
+class uvc_xu_control_query(ctypes.Structure):
+    _fields_ = [
+        ('unit', ctypes.c_uint8),
+        ('selector', ctypes.c_uint8),
+        ('query', ctypes.c_uint8),		# Video Class-Specific Request Code,
+                                        # defined in linux/usb/video.h A.8. 
+        ('size', ctypes.c_uint16),
+        ('data', ctypes.c_void_p),
+    ]
+
+UVCIOC_CTRL_QUERY = _IOWR('u', 0x21, uvc_xu_control_query)
+
+# A.8. Video Class-Specific Request Codes
+UVC_RC_UNDEFINED = 0x00
+UVC_SET_CUR      = 0x01
+UVC_GET_CUR      = 0x81
+UVC_GET_MIN      = 0x82
+UVC_GET_MAX      = 0x83
+UVC_GET_RES      = 0x84
+UVC_GET_LEN      = 0x85
+UVC_GET_INFO     = 0x86
+UVC_GET_DEF      = 0x87
+
+
+UVCX_VIDEO_CONFIG_PROBE		 = 0x01
+UVCX_VIDEO_CONFIG_COMMIT	 = 0x02
+
+# UVC EU1 extension GUID 23e49ed0-1178-4f31-ae52-d2fb8a8d3b48
+UVC_EU1_GUID = b'\xd0\x9e\xe4\x23\x78\x11\x31\x4f\xae\x52\xd2\xfb\x8a\x8d\x3b\x48'
+
+# Razer Kiyo Pro specific registers and values
+
+UVC_KIYO_PRO_AF_RESPONSIVE = b'\xff\x06\x00\x00\x00\x00\x00\x00'
+UVC_KIYO_PRO_AF_PASSIVE =    b'\xff\x06\x01\x00\x00\x00\x00\x00'
+
+UVC_KIYO_PRO_HDR_OFF =       b'\xff\x02\x00\x00\x00\x00\x00\x00'
+UVC_KIYO_PRO_HDR_ON =        b'\xff\x02\x01\x00\x00\x00\x00\x00'
+
+UVC_KIYO_PRO_HDR_DARK =      b'\xff\x07\x00\x00\x00\x00\x00\x00'
+UVC_KIYO_PRO_HDR_BRIGHT =    b'\xff\x07\x01\x00\x00\x00\x00\x00'
+
+UVC_KIYO_PRO_FOV_WIDE =       b'\xff\x01\x00\x03\x00\x00\x00\x00'
+UVC_KIYO_PRO_FOV_MEDIUM_PRE = b'\xff\x01\x00\x03\x01\x00\x00\x00'
+UVC_KIYO_PRO_FOV_MEDIUM =     b'\xff\x01\x01\x03\x01\x00\x00\x00'
+UVC_KIYO_PRO_FOV_NARROW_PRE = b'\xff\x01\x00\x03\x02\x00\x00\x00'
+UVC_KIYO_PRO_FOV_NARROW =     b'\xff\x01\x01\x03\x02\x00\x00\x00'
+
+# Unknown yet, the synapse sends it in start
+UVC_KIYO_PRO_UNKNOWN =       b'\xff\x04\x00\x00\x00\x00\x00\x00'
+
+# save previous values to the camera
+UVC_KIYO_PRO_SAVE =          b'\xc0\x03\xa8\x00\x00\x00\x00\x00'
+
+UVC_KIYO_PRO_LOAD =          b'\x00\x00\x00\x00\x00\x00\x00\x00'
+
+def to_buf(b):
+    return ctypes.create_string_buffer(b)
+
+def get_length_xu_control(fd, unit_id, selector):
+    length = ctypes.c_uint16(0)
+
+    xu_ctrl_query = uvc_xu_control_query()
+    xu_ctrl_query.unit = unit_id
+    xu_ctrl_query.selector = selector
+    xu_ctrl_query.query = UVC_GET_LEN
+    xu_ctrl_query.size = 2 # sizeof(length)
+    xu_ctrl_query.data = ctypes.cast(ctypes.pointer(length), ctypes.c_void_p)
+
+    try:
+       ioctl(fd, UVCIOC_CTRL_QUERY, xu_ctrl_query)
+    except Exception as e:
+        logging.warning(f'UVCIOC_CTRL_QUERY (GET_LEN) - Fd: {fd} - Error: {e}')
+
+    return length
+
+def query_xu_control(fd, unit_id, selector, query, data):
+    len = get_length_xu_control(fd, unit_id, selector)
+
+    xu_ctrl_query = uvc_xu_control_query()
+    xu_ctrl_query.unit = unit_id
+    xu_ctrl_query.selector = selector
+    xu_ctrl_query.query = query
+    xu_ctrl_query.size = len
+    xu_ctrl_query.data = ctypes.cast(ctypes.pointer(data), ctypes.c_void_p)
+
+    try:
+        ioctl(fd, UVCIOC_CTRL_QUERY, xu_ctrl_query)
+    except Exception as e:
+        logging.warning(f'UVCIOC_CTRL_QUERY ({query}) - Fd: {fd} - Error: {e}')
+
+# the usb device descriptors file contains the descriptors in a binary format
+# the byte before the extension guid is the extension unit id
+def find_unit_id_in_sysfs(device, guid):
+    if os.path.islink(device):
+        device = os.readlink(device)
+    device = os.path.basename(device)
+    descfile = f'/sys/class/video4linux/{device}/../../../descriptors'
+    if not os.path.isfile(descfile):
+        return 0
+
+    try:
+        with open(descfile, 'rb') as f:
+            descriptors = f.read()
+            guid_start = descriptors.find(guid)
+            if guid_start > 0:
+                return descriptors[guid_start - 1]
+    except Exception as e:
+        logging.warning(f'Failed to read uvc xu unit id from {descfile}: {e}')
+
+    return 0
+
+def find_usb_ids_in_sysfs(device):
+    if os.path.islink(device):
+        device = os.readlink(device)
+    device = os.path.basename(device)
+    vendorfile = f'/sys/class/video4linux/{device}/../../../idVendor'
+    productfile = f'/sys/class/video4linux/{device}/../../../idProduct'
+    if not os.path.isfile(vendorfile) or not os.path.isfile(productfile):
+        return ''
+
+    vendor = ''
+    try:
+        with open(vendorfile, 'r') as f:
+            vendor = f.read().strip()
+    except Exception as e:
+        logging.warning(f'Failed to read usb vendor id from {vendorfile}: {e}')
+
+    product = ''
+    try:
+        with open(productfile, 'r') as f:
+            product = f.read().strip()
+    except Exception as e:
+        logging.warning(f'Failed to read usb product id from {productfile}: {e}')
+
+    return vendor + ':' + product
+
+
+class UVCXKiyoProCtrls:
+    def __init__(self, device, fd):
+        self.device = device
+        self.fd = fd
+        self.unit_id = find_unit_id_in_sysfs(device, UVC_EU1_GUID)
+        self.usb_ids = find_usb_ids_in_sysfs(device)
+        self.get_device_controls()
+
+    def supported(self):
+        return self.unit_id != 0 and self.usb_ids == '1532:0e05'
+
+    def get_device_controls(self):
+        if not self.supported():
+            self.ctrls = []
+            return
+
+        # getting the values doesn't work in this way, maybe later in newer firmwares
+        #current = uvcx.to_buf(uvcx.UVC_KIYO_PRO_LOAD)
+        #uvcx.query_xu_control(self.fd, self.unit_id, uvcx.UVCX_VIDEO_CONFIG_COMMIT, uvcx.UVC_GET_CUR, current)
+        #print('current:', list(current))
+
+        self.ctrls = [
+            KIYOCtrl(
+                'af_mode',
+                {
+                    UVC_KIYO_PRO_AF_PASSIVE: 'passive',
+                    UVC_KIYO_PRO_AF_RESPONSIVE: 'responsive',
+                }
+            ),
+            KIYOCtrl(
+                'hdr',
+                {
+                    UVC_KIYO_PRO_HDR_OFF: 'off',
+                    UVC_KIYO_PRO_HDR_ON: 'on',
+                }
+            ),
+            KIYOCtrl(
+                'hdr_mode',
+                {
+                    UVC_KIYO_PRO_HDR_BRIGHT: 'bright',
+                    UVC_KIYO_PRO_HDR_DARK: 'dark',
+                }
+            ),
+            KIYOCtrl(
+                'fov',
+                {
+                    UVC_KIYO_PRO_FOV_WIDE: 'wide',
+                    UVC_KIYO_PRO_FOV_MEDIUM: 'medium',
+                    UVC_KIYO_PRO_FOV_NARROW: 'narrow',
+                }
+            ),
+        ]
+        self.befores = {
+            UVC_KIYO_PRO_FOV_MEDIUM: UVC_KIYO_PRO_FOV_MEDIUM_PRE,
+            UVC_KIYO_PRO_FOV_NARROW: UVC_KIYO_PRO_FOV_NARROW_PRE,
+        }
+    
+    def print_ctrls(self):
+        for c in self.ctrls:
+            for k, v in c.menu.items():
+                print(f'{c.name}={v}')
+
+    def setup_ctrls(self, params):
+        for k, v in params.items():
+            ctrl = find_by_name(self.ctrls, k)
+            if ctrl == None:
+                logging.warning(f'UVCXKiyoProCtrls: can\'t find {k} control in {[c.name for c in self.ctrls]}')
+                continue
+            menukey = find_by_value(ctrl.menu, v)
+            if menukey == None:
+                logging.warning(f'UVCXKiyoProCtrls: can\'t find {v} in {[v for v in ctrl.menu.values()]}')
+                continue
+            ctrl.value = menukey
+
+            before = self.befores.get(ctrl.value)
+            if before:
+                query_xu_control(self.fd, self.unit_id, UVCX_VIDEO_CONFIG_PROBE, UVC_SET_CUR, to_buf(before))
+            
+            query_xu_control(self.fd, self.unit_id, UVCX_VIDEO_CONFIG_PROBE, UVC_SET_CUR, to_buf(ctrl.value))
+
+        query_xu_control(self.fd, self.unit_id, UVCX_VIDEO_CONFIG_PROBE, UVC_SET_CUR, to_buf(UVC_KIYO_PRO_SAVE))
+
+
+
+
+def find_by_name(ctrls, name):
+    for c in ctrls:
+        if name == c.name:
+            return c
+    return None
+
+def find_by_value(menu, value):
+    for k, v in menu.items():
+        if v == value:
+            return k
+    return None
+
+class KIYOCtrl:
+    def __init__(self, name, menu = None):
+        self.name = name
+        self.value = None
+        self.menu = menu
+
+
+def usage():
+    print(f'usage: python3 {sys.argv[0]} [--help] [-d DEVICE] [--list] [-c CONTROLS]\n')
+    print(f'optional arguments:')
+    print(f'  -h, --help    show this help message and exit')
+    print(f'  -d DEVICE     use DEVICE, default /dev/video0')
+    print(f'  -l, --list    list the controls and values')
+    print(f'  -c CONTROLS   set CONTROLS (eg.: hdr=on,fov=wide)')
+    print()
+    print(f'example:')
+    print(f'  python3 {sys.argv[0]} -c hdr=on,hdr_mode=dark,af_mode=passive,fov=wide')
+
+
+try:
+    arguments, values = getopt.getopt(sys.argv[1:], 'hd:lc:', ['help', 'list'])
+except getopt.error as err:
+    print(err)
+    usage()
+    sys.exit(2)
+
+if len(arguments) == 0:
+    usage()
+    sys.exit(0)
+
+list_controls = False
+device = '/dev/video0'
+controls = ''
+
+for current_argument, current_value in arguments:
+    if current_argument in ('-h', '--help'):
+        usage()
+        sys.exit(0)
+    elif current_argument in ('-d', '--device'):
+        device = current_value
+    elif current_argument in ("-l", "--list"):
+        list_controls = True
+    elif current_argument in ('-c'):
+        controls = current_value
+
+try:
+    fd = os.open(device, os.O_RDWR, 0)
+except Exception as e:
+    logging.error(f'os.open({device}, os.O_RDWR, 0) failed: {e}')
+    sys.exit(2)
+
+kiyo_pro = UVCXKiyoProCtrls(device, fd)
+if not kiyo_pro.supported():
+    logging.error(f'{device} is not a Kiyo Pro')
+    sys.exit(3)
+
+if list_controls:
+    kiyo_pro.print_ctrls()
+
+if controls != '':
+    ctrlsmap = {}
+    for control in controls.split(','):
+        kv = control.split('=', maxsplit=1)
+        if len(kv) != 2:
+            logging.warning(f'invalid value: {control}')
+            continue
+        ctrlsmap[kv[0]]=kv[1]
+
+    kiyo_pro.setup_ctrls(ctrlsmap)
